3fe3ef117e2d34fcd61c0b40f8b98483f04bff5f
Stevedow99 <44956078+Stevedow99@users.noreply.github.com>
2018-11-13 12:53:24 -0500

    Update MetroWDC.js

--- metro-wdc/MetroWDC.js
+++ metro-wdc/MetroWDC.js
@@ -5,45 +5,28 @@
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [{
-            id: "IncidentID",
+            id: "DateUpdated",
             dataType: tableau.dataTypeEnum.string
         }, {
             id: "Description",
-            alias: "Incident Description",
+            alias: "Description",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "LinesAffected",
-            alias: "LinesAffected",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "StartLocationFullName",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "EndLocationFullName",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "PassengerDelay",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "DelaySeverity",
+            id: "IncidentID",
+            alias: "IncidentID",
             dataType: tableau.dataTypeEnum.string
         }, {
             id: "IncidentType",
+            alias: "Incident Type",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "EmergencyText",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "LinesAffected",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "DateUpdated",
+            id: "RoutesAffected",
             dataType: tableau.dataTypeEnum.string
         }];
 
         var tableSchema = {
-            id: "MWTADataFeed",
-            alias: "WMTA Rail Incidents",
+            id: "BusIncidents",
+            alias: "WMTA Bus Incidents",
             columns: cols
         };
 
@@ -54,33 +37,27 @@
             var settings = {
               "async": true,
               "crossDomain": true,
-              "url": "https://api.wmata.com/Incidents.svc/json/Incidents",
+              "url": "https://api.wmata.com/Incidents.svc/json/BusIncidents",
               "method": "GET",
               "headers": {
-                "api_key": "29fb2c42b2e54ffe9226628a3010b4f3",
-                "cache-control": "no-cache",
-                "Postman-Token": "6844e148-70e6-480d-a35d-a7a13cea42a4"
+                "api_key": "29fb2c42b2e54ffe9226628a3010b4f3"
               }
-            }
+            };
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        $.ajax(settings).done(function(resp) {
+        $.ajax(settings).done(function(response) {
+            console.log(response)
                var tableData = [];
 
             // Iterate over the JSON object
-            for (var i = 0, len = resp.length; i < len; i++) {
+            for (var i = 0, len = response.length; i < len; i++) {
                 tableData.push({
-                    "IncidentID": resp[i].IncidentID,
-                    "Description": resp[i].Description,
-                    "StartLocationFullName": resp[i].StartLocationFullName,
-                    "EndLocationFullName": resp[i].EndLocationFullName,
-                    "PassengerDelay": resp[i].PassengerDelay,
-                    "DelaySeverity": resp[i].DelaySeverity,
-                    "IncidentType": resp[i].IncidentType,
-                    "EmergencyText": resp[i].EmergencyText,
-                    "LinesAffected": resp[i].LinesAffected,
-                    "DateUpdated": resp[i].DateUpdated
+                    "DateUpdated": response[i].DateUpdated,
+                    "Description": response[i].Description,
+                    "IncidentID": response[i].IncidentID,
+                    "IncidentType": response[i].IncidentType,
+                    "RoutesAffected": resp[i].RoutesAffected
                 });
             }
 
@@ -94,7 +71,7 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "WMTA Rail Incidents"; // This will be the data source name in Tableau
+            tableau.connectionName = "WMTA Bus Incidents"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
