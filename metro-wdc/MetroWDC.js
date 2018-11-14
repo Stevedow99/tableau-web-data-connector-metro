28f322e623104ad878e58421ab7b803f6d3e0b42
Stevedow99 <44956078+Stevedow99@users.noreply.github.com>
2018-11-14 15:59:19 -0500

    Update MetroWDC.js

--- metro-wdc/MetroWDC.js
+++ metro-wdc/MetroWDC.js
@@ -1,10 +1,12 @@
-(function() {
-    // Create the connector object
-    var myConnector = tableau.makeConnector();
+(function () {
+   
+    $(document).ready(function () {
+         var myConnector = tableau.makeConnector();
 
-    // Define the schema
-    myConnector.getSchema = function(schemaCallback) {
-        var cols = [{
+    myConnector.getSchema = function (schemaCallback) {
+        var cols = [
+
+         {
             id: "DateUpdated",
             dataType: tableau.dataTypeEnum.string
         }, {
@@ -23,69 +25,66 @@
             id: "RoutesAffected",
             alias: "Routes Affected",
             dataType: tableau.dataTypeEnum.string
-        }];
+        }
 
+
+        ];
+    
         var tableSchema = {
             id: "BusIncidents",
             alias: "WMTA Bus Incidents",
             columns: cols
         };
-
+    
         schemaCallback([tableSchema]);
     };
 
+    myConnector.getData = function (table, doneCallback) {
+        
+        const url = 'http://localhost:8889/api.wmata.com/Incidents.svc/json/BusIncidents'
+        
 
 
-
-    // Download the data
-    myConnector.getData = function(table, doneCallback) {
         $.ajax({
-              "async": true,
-              "crossDomain": true,
-              "url": "https://api.wmata.com/Incidents.svc/json/BusIncidents",
-              "method": "GET",
-              "headers": {
-                "api_key": "29fb2c42b2e54ffe9226628a3010b4f3"
-              }
-            }).done(function(response){
-                jsondata = response
-            console.log(response)
-               var tableData = [];
+            async: true,
+            crossDomain: true,
+            dataType: "json",
+            url: url,
+            data: {},
+            headers: {
+                "api_key": "29fb2c42b2e54ffe9226628a3010b4f3",
+                "Access-Control-Allow-Origin": "*"
+                     },
 
-          
-
-            // Iterate over the JSON object
-             for (var i = 0, len = response.BusIncidents.length; i < len; i++) 
-             
-             {
-               tableData.push({
-                    "DateUpdated": response.BusIncidents[i].DateUpdated,
-                    "Description": response.BusIncidents[i].Description,
-                    "IncidentID": response.BusIncidents[i].IncidentID,
-                    "IncidentType": response.BusIncidents[i].IncidentType,
-                    "RoutesAffected": response.BusIncidents[i].RoutesAffected
-                });
+             success: function(data) {
+                const allRows = []
+                const timeSeries = data['BusIncidents']
+                const keys = Object.keys(timeSeries)
+                for (var i = 0 ; i < keys.length ; i++) {
+                    const key = keys[i]
+                    const actual = timeSeries[key]
+                    const expected = {}
+                    expected.DateUpdated = actual['DateUpdated']
+                    expected.Description = actual['Description']
+                    expected.IncidentID = actual['IncidentID']
+                    expected.IncidentType = actual['IncidentType']
+                    expected.RoutesAffected = actual['RoutesAffected']
+                    allRows.push(expected)
+                    }
+                    
+                table.appendRows(allRows)
+                doneCallback();
             }
-
-
-
-            table.appendRows(tableData);
-            doneCallback();
-
-
         });
     };
 
     tableau.registerConnector(myConnector);
 
-    // Create event listeners for when the user submits the form
-    $(document).ready(function() {
-        $("#submitButton").click(function() {
-            tableau.connectionName = "WMTA Bus Incidents"; // This will be the data source name in Tableau
-            tableau.submit(); // This sends the connector object to Tableau
-
+        $("#submitButton").click(function () {
+    
+  
+            tableau.connectionName = "WMTA Bus Incidents";
+            tableau.submit();
         });
     });
 })();
-
-
